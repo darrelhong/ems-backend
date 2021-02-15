06e70399d3506cee6545529cd439feb4d3aee905
Lin LiLi <e0318915@u.nus.edu>
2021-02-16 01:38:11 +0800

    upload profile pic branch, tested on post man.

---
+++ src/main/java/com/is4103/backend/util/errors/MyFileNotFoundException.java
@@ -0,0 +1,16 @@
+package com.is4103.backend.util.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class MyFileNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public MyFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
