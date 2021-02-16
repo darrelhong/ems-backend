9e379df56036403873be77a820f6260a84b325c6
keithhjc <keithhjc@gmail.com>
2021-02-16 15:37:48 +0800

    Remove duplicate

--- src/main/java/com/is4103/backend/model/Event.java
+++ src/main/java/com/is4103/backend/model/Event.java
@@ -17,8 +17,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.CascadeType;
 
 import lombok.Data;
@@ -31,7 +29,6 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long eid;
 
-    @JsonIgnoreProperties("events")
     @ManyToOne
     @JsonIgnoreProperties("events")
     private EventOrganiser eventOrganiser;
