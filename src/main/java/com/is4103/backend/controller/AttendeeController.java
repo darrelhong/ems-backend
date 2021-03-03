984e3b9b6c5cb46e0db004ae26ae7abc777fe44b
Cheng Wun Jing <chengwunjing@outlook.com>
2021-03-04 01:53:09 +0800

    added attendee backend

---
+++ src/main/java/com/is4103/backend/controller/AttendeeController.java
@@ -0,0 +1,51 @@
+package com.is4103.backend.controller;
+
+import javax.validation.Valid;
+
+import com.is4103.backend.dto.SignupRequest;
+import com.is4103.backend.dto.SignupResponse;
+import com.is4103.backend.service.AttendeeService;
+import com.is4103.backend.service.UserService;
+import com.is4103.backend.util.errors.UserAlreadyExistsException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/attendee")
+public class AttendeeController {
+
+    @Autowired
+    private AttendeeService atnService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/register")
+    public SignupResponse registerNewAttendee(@RequestBody @Valid SignupRequest signupRequest) {
+        // return 
+        try{
+    
+            if (userService.emailExists(signupRequest.getEmail())) {
+                throw new UserAlreadyExistsException("Account with email " + signupRequest.getEmail() + " already exists");
+            }else{
+            
+           atnService.registerNewAttendee(signupRequest, false);
+    
+            }
+        
+            }
+            catch(UserAlreadyExistsException userAlrExistException){
+               return new SignupResponse("alreadyExisted");
+            }
+    
+           
+            return new SignupResponse("success");
+    }
+
+
+    
+}
---
+++ src/main/java/com/is4103/backend/service/AttendeeService.java
@@ -0,0 +1,79 @@
+package com.is4103.backend.service;
+
+import com.is4103.backend.dto.SignupRequest;
+import com.is4103.backend.model.Attendee;
+import com.is4103.backend.model.Role;
+import com.is4103.backend.model.RoleEnum;
+import com.is4103.backend.repository.AttendeeRepository;
+import com.is4103.backend.util.errors.UserAlreadyExistsException;
+import com.is4103.backend.util.registration.OnRegistrationCompleteEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
+@Service
+public class AttendeeService {
+
+    @Autowired
+    private AttendeeRepository atnRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    @Transactional
+    public Attendee registerNewAttendee(SignupRequest signupRequest, boolean enabled)
+            throws UserAlreadyExistsException {
+        if (userService.emailExists(signupRequest.getEmail())) {
+            throw new UserAlreadyExistsException("Account with email " + signupRequest.getEmail() + " already exists");
+        }
+
+        Attendee atn = new Attendee();
+        atn.setName(signupRequest.getName());
+        atn.setEmail(signupRequest.getEmail());
+
+        atn.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
+        Role role = roleService.findByRoleEnum(RoleEnum.ATND);
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        atn.setRoles(roles);
+
+        if (enabled) {
+            atn.setEnabled(true);
+        }
+
+        if (enabled) {
+            atn.setEnabled(true);
+        }
+
+        atn = atnRepository.save(atn);
+
+        if (!atn.isEnabled()) {
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(atn));
+        }
+
+        return atn;
+    }
+
+
+
+
+
+
+    
+}
