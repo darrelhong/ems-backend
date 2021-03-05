c0eada638b4b8b359cb87c2100f8e6a42d7e2340
justinlgh <54320651+justinlgh@users.noreply.github.com>
2021-03-05 17:06:34 +0800

    added method updateAttendee

--- src/main/java/com/is4103/backend/service/AttendeeService.java
+++ src/main/java/com/is4103/backend/service/AttendeeService.java
@@ -1,6 +1,7 @@
 package com.is4103.backend.service;
 
 import com.is4103.backend.dto.SignupRequest;
+import com.is4103.backend.dto.UpdateAttendeeRequest;
 import com.is4103.backend.model.Attendee;
 import com.is4103.backend.model.Role;
 import com.is4103.backend.model.RoleEnum;
@@ -78,6 +79,18 @@ public class AttendeeService {
         }
 
         return atn;
+    }
+
+    @Transactional
+    public Attendee updateAttendee(Attendee user, UpdateAttendeeRequest updateAttendeeRequest) {
+
+        user.setName(updateAttendeeRequest.getName());
+        user.setDescription(updateAttendeeRequest.getDescription());
+        user.setAddress(updateAttendeeRequest.getAddress());
+        user.setPhonenumber(updateAttendeeRequest.getPhonenumber());
+        user.setCategoryPreferences(updateAttendeeRequest.getCategoryPreferences());
+
+        return atnRepository.save(user);
     }
 
 }
