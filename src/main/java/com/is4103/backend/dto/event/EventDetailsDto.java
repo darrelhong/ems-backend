66b328d4f3d42a4156e2315738b49e78c0a47ac4
darrelhong <darrel.hong@gmail.com>
2021-04-08 18:12:33 +0800

    add booth data to event details

--- src/main/java/com/is4103/backend/dto/event/EventDetailsDto.java
+++ src/main/java/com/is4103/backend/dto/event/EventDetailsDto.java
@@ -40,7 +40,49 @@ public interface EventDetailsDto {
 
     EventOrganiserDetails getEventOrganiser();
 
+    List<SellerProfileSummary> getSellerProfiles();
+
     interface EventOrganiserDetails {
         String getName();
+    }
+
+    interface SellerProfileSummary {
+        Long getId();
+
+        String getDescription();
+
+        List<BoothSummary> getBooths();
+
+        List<String> getBrochureImages();
+    }
+
+    interface BoothSummary {
+        Long getId();
+
+        List<ProductSummary> getProducts();
+
+        Integer getBoothNumber();
+
+        String getDescription();
+
+        BoothSellerProfileSummary getSellerProfile();
+    }
+
+    interface BoothSellerProfileSummary {
+        BusinessPartnerSummary getBusinessPartner();
+    }
+
+    interface BusinessPartnerSummary {
+        String getName();
+    }
+
+    interface ProductSummary {
+        Long getPid();
+
+        String getName();
+
+        String getDescription();
+
+        String getImage();
     }
 }
