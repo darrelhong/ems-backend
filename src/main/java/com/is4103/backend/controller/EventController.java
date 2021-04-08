d064294a458c8d03eb62e679b37c1b78f5b8c24a
darrelhong <darrel.hong@gmail.com>
2021-04-08 13:34:13 +0800

    fix modelmapper autowired

--- src/main/java/com/is4103/backend/controller/EventController.java
+++ src/main/java/com/is4103/backend/controller/EventController.java
@@ -30,8 +30,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +38,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.BeanUtils;
 
 @RestController
 @RequestMapping(path = "/event")
@@ -58,6 +55,7 @@ public class EventController {
     @Autowired
     private BusinessPartnerService bpService;
 
+    @Autowired
     private ModelMapper modelMapper;
 
     @GetMapping(path = "/all")
