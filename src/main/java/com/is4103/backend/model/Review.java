bfaeb1bacb22c3a9e5fadfa62c8aee24e5817917
keithhjc <keithhjc@gmail.com>
2021-04-13 16:22:21 +0800

    added rate endpt for event card to fetch latest rating

--- src/main/java/com/is4103/backend/model/Review.java
+++ src/main/java/com/is4103/backend/model/Review.java
@@ -2,6 +2,7 @@ package com.is4103.backend.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,8 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-
 
 @Entity
 @Data
@@ -26,11 +28,14 @@ public class Review {
     private Attendee attendee;
 
     @ManyToOne
+    @JsonIgnore
     private BusinessPartner partner;
 
     private String reviewText;
 
     @ManyToOne
+    @JsonIgnore
     private Event event;
+
     private String reviewDateTime;
 }
